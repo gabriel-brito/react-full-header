2ce86a31f12419856e83c569f836473293971f98
Gabriel Santos <mfkrgabriel@gmail.com>
2019-03-29 16:49:34 -0300

    -20190329

--- stories/Main.js
+++ stories/Main.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import Component from '../src/Main'; // This is our component
+import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Component', module)
-    .add('with lyef name', () => (
-        <Component name="lyef" />
-    ))
-    .add('with another name', () => (
-        <Component name="another" />
-    ))
+storiesOf('React Full Header', module)
+  .add('With title', () => {
+    <FullHeader title="JS com TDD" />
+  })
+  .add('With title and subtitle', () => {
+    <FullHeader 
+      title="JS com TDD"
+      subtitle="what is tested may never break"
+    />
+  })
+  .add('With title, subtitle and bgColor', () => {
+    <FullHeader 
+      title="JS com TDD"
+      subtitle="what is tested may never break"
+      bgColor="3299BB"
+    />
+  })
+  .add('With title, subtitle, bgColor and textColor', () => {
+    <FullHeader 
+      title="JS com TDD"
+      subtitle="what is tested may never break"
+      bgColor="3299BB"
+      textColor="#FF9900"
+    />
+  })
+  .add('With title, subtitle, bgColor, textColor and font', () => {
+    <FullHeader 
+      title="JS com TDD"
+      subtitle="what is tested may never break"
+      bgColor="3299BB"
+      textColor="#FF9900"
+      font="cursive"
+    />
+  })
+  .add('With title, subtitle, bgImg', () => {
+    <FullHeader 
+      title="JS com TDD"
+      subtitle="what is tested may never break"
+      bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
+    />
+  })
+  .add('with title, subtitle and video', () => (
+    <FullHeader
+      title="JS com TDD"
+      subtitle="what is tested may never break"
+      bgColor="#EBE9EB"
+      textColor="#3299BB"
+      video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
+    />
+  ));
--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -1,22 +1,61 @@
 import React from 'react';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 import { shallow } from 'enzyme';
-import Component from '../../src/Main';
+import FullHeader from '../../src/Main';
 
-describe('Component Hello', () => {
-   it('should have h1 to display the Hello', () => {
-       const wrapper = shallow(<Component name='lyef' />);
-       expect(wrapper.find('h1')).to.have.length(1);
-   });
+chai.use(chaiEnzyme());
 
-    it('should have props for name', () => {
-        const wrapper = shallow(<Component name='lyef' />);
-        expect(wrapper.props().name).to.be.defined;
+describe('FullHeader ', () => {
+  it('should have header tag when mount', () => {
+    const component = shallow(<FullHeader />);
+    expect(component.find('header')).to.have.length(1);
+  });
+
+  context('Title', () => {
+    it('should have h1 tag when title is passed', () => {
+      const component = shallow(<FullHeader title="JS com TDD" />);
+      expect(component.find('h1')).to.have.length(1);
     });
 
-    it('should create a correct DOM structure', () => {
-        const wrapper = shallow(<Component name='lyef' />);
-        const componentMock = shallow(<h1>Hello lyef!</h1>);
-        expect(wrapper.html()).to.be.equal(componentMock.html());
+    it('should not have h1 tag when title is not passed', () => {
+      const component = shallow(<FullHeader />);
+      expect(component.find('h1')).to.have.length(0);
     });
+
+    it('should have h1 tag with title passed by props', () => {
+      const component = shallow(<FullHeader title="JS com TDD" />);
+      expect(component.find('h1').props().children).to.be.equal("JS com TDD");
+    });
+  });
+
+  context('Subtitle', () => {
+    it('should have h2 tag when subtitle is passed', () => {
+      const component = shallow(<FullHeader subtitle="What is tested may never break" />);
+      expect(component.find('h2')).to.have.length(1);
+    });
+
+    it('should not have h2 tag when subtitle is not passed', () => {
+      const component = shallow(<FullHeader />);
+      expect(component.find('h2')).to.have.length(0);
+    });
+
+    it('should have h2 tag with subtitle passed by props', () => {
+      const component = shallow(<FullHeader subtitle="What is tested may never break" />);
+      expect(component.find('h2').props().children).to.be.equal("What is tested may never break");
+    });
+  });
+
+  context('bgColor', () => {
+    it('should have background #ccc when none is passed', () => {
+      const component = shallow(<FullHeader />);
+     expect(component).to.have.style('background-color').equal('#ccc');
+    });
+
+    it('should have background #FFF when #FFF is passed', () => {
+      const component = shallow(<FullHeader bgColor="#FFF" />);
+     expect(component).to.have.style('background-color').equal('#FFF');
+    });
+  });
+
 });
